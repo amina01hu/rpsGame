9e3a3a61a7a53b62a2cb01db7f986ccfa1a099fa
amina <amina01hu@gmail.com>
2022-06-02 19:45:27 -0400

    add rest call

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 // import and load the express module
 const express = require('express'); 
 const app = express(); // -> has methods for HTTP requests, middleware, views/templates, and modifying settings
-const port = 3000; // -> declaring port 3000
+const port = process.env.PORT || 3000; // -> env variable or 3000
 
-app.get('/', function(req, res){ // -> this is a route definition for http://localhost:3000/ (for example, will be changed)
+app.get('/', (req, res) => { // -> this is a route definition for http://localhost:3000/ (for example, will be changed)
     res.send("Hello World, testing backend");
 });
 
-app.listen(port, function(){ //-> starts server on specified port
+app.get('/api/users', (req, res) => {
+    res.send({"user1": "Amina", "user2":"Johnny"});
+})
+
+app.listen(port, () =>{ //-> starts server on specified port
     console.log(`App listening on port ${port}!`, "Also listening on " + port);
 });
