7834f912491cdb83da1fa7cb908b0edcfbfc639e
amina <amina01hu@gmail.com>
2022-06-18 20:29:56 -0400

    connection between db

--- rps-app/backend/server.js
+++ rps-app/backend/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express(); // -> has methods for HTTP requests, middleware, views/templates, and modifying settings
 const mongoose = require('mongoose'); //used to connect to the databse
 const dotenv = require('dotenv'); //used for env variables
-const routes = require('./routes/routes'); //call routes.js after receiving call here
 const cors = require('cors');
 const port = process.env.PORT || 3000; //
+const userModel = require('./schema/signupmodel');
 
 dotenv.config()
 
@@ -14,7 +14,24 @@ mongoose.connect(process.env.DATABASE_ACCESS, () =>{
 })
 app.use(express.json()); //-> activates body parser
 app.use(cors()); //-> activates cors
-app.use('/api', routes); //activates router api after call (/api/sign)
+
+app.get("/usersList", (req, res) =>{
+    userModel.find({}, (err, result) => {
+        if(err){
+            res.json(err);
+        }else{
+            res.json(result);
+        }
+    }); //returns all data inside the current collection
+})
+
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const signupuser = new userModel(user);
+    await signupuser.save();
+    res.json(user);
+})
+
 app.listen(port, () =>{ //-> starts server on specified port
     console.log(`App listening on port ${port}!`);
 });
